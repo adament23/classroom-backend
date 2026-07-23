daf6e4f63dde8088973c35ac1a5939834a345b4f
adament23 <sharmaadament23@gmail.com>
2026-07-24 03:16:07 +0530

    implement suggestions from coderabit

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -38,7 +38,7 @@ router.get("/" , async (req, res) => {
         const whereClause = filterConditions.length > 0 ? and(...filterConditions) : undefined;
 
         const countResult = await db
-            .select({ count: sql<number>`count(*)`})
+            .select({ count: sql<number>`count(*)`.mapWith(Number)})
             .from(subjects)
             .leftJoin(departments , eq(subjects.departmentId, departments.id))
             .where(whereClause)
